fe7220595c064b2c79e1749c5ceb490218e972bd
Youssef <yauravy@gmail.com>
2022-11-22 01:00:32 +0100

    email configuration

--- src/Notification/Sender.php
+++ src/Notification/Sender.php
@@ -8,9 +8,24 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class Sender
 {
-    public function sendUserNotificationToAdmin(UserInterface $user)
+    protected $mailer;
+
+    public function __construct(MailerInterface $mailer)
     {
-        file_put_contents('debug.txt', $user->getEmail());
+        $this->mailer = $mailer;
+    }
+
+    public function sendUserNotificationToAdmin(UserInterface $user): void
+    {
+        //file_put_contents('debug.txt', $user->getEmail());
+
+        $message = new Email();
+        $message->from('accounts@trips.com')
+            ->to('admin@trips.com')
+            ->subject('new account created on trips.com')
+            ->html('<h1>New Account Created</h1>email:'.$user->getEmail());
+
+        $this->mailer->send($message);
 
     }
 }
